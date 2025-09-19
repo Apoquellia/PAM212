e407e4c9be6ee629a73ceaa4c67b3251554ab9a9
José Luis Huante Ríos <124051243@upq.edu.mx>
2025-09-18 22:14:31 -0600

    Ejercicio 3

---
+++ Practica1/ArrowSaludo.js
@@ -0,0 +1,7 @@
+const saludoPersonalizado = (nombre, edad) => 
+    {
+    let saludo = "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+    return saludo
+    }
+
+console.log(saludoPersonalizado("José",19))
