5365ac822e4985f7a2b4c07f6c24f9b0edc7cdbb
José Luis Huante Ríos <124051243@upq.edu.mx>
2025-11-12 18:32:20 -0600

    BottomSheet: Completada

---
+++ intro/screens/BottomSheetScreen.js
@@ -0,0 +1,45 @@
+import React, {useRef, useMemo } from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export default function BottomSheetScreen() {
+  const bottomSheetRef = useRef(null);
+  const snap = useMemo(() => [1, '25%', '85%']);
+  const abrir = () =>{
+    bottomSheetRef.current?.expand();
+  };
+
+  return (
+    <GestureHandlerRootView>
+    <View style={styles.container}>
+      <Button title='Abrir' onPress={abrir}/>
+    </View>
+    <BottomSheet
+      ref = {bottomSheetRef}
+      snapPoints = {snap}
+      enablePanDownToClose = {true}
+      backgroundStyle = {styles.BSheet}
+      >
+        <BottomSheetView>
+          <Text style = {styles.BText}>BottomSheet</Text>
+        </BottomSheetView>
+    </BottomSheet>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  BSheet:{
+    backgroundColor:'#840082ff'
+  },
+  BText:{
+    fontSize: 50
+  }
+});
